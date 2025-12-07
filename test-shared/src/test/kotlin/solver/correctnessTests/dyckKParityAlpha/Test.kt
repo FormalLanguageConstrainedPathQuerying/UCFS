751da7004a8ead09e6a351395f15f885f0262ef9
bash olya <bash.olya7@gmail.com>
2025-12-07 20:44:22 +0300

    make parity test smaller

--- test-shared/src/test/kotlin/solver/correctnessTests/dyckKParityAlpha/Test.kt
+++ test-shared/src/test/kotlin/solver/correctnessTests/dyckKParityAlpha/Test.kt
@@ -3,7 +3,7 @@ package solver.correctnessTests.dyckKParityAlpha
 import org.junit.jupiter.api.Test
 import solver.correctnessTests.AbstractCorrectnessTest
 
-class DyckKParityAlphaTest : AbstractCorrectnessTest() {
+class DyckKParityTest : AbstractCorrectnessTest() {
     @Test
     override fun checkTreeCorrectnessForGrammar() {
         runTests(dyckAlphaGrammarKParity())
